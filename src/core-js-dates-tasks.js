787e1a5c78adf89e23472cf90b445c64fbf8dae5
Darya Kolenchenko <daryakolenchenko@Daryas-MacBook-Pro.local>
2025-02-26 12:48:45 +0100

    feat: pass 14 tests

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -354,8 +354,10 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  const check = new Date(year, 1, 29);
+  return check.getMonth() === 1;
 }
 
 module.exports = {
